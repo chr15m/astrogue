de86ab9150b5b84bc46a03ed978ec872dea88aea
Chris McCormick <chris@mccormick.cx>
2017-04-24 20:22:43 +0800

    2/3rds view ratio.

--- src/cljs/astrogue/renderer.cljs
+++ src/cljs/astrogue/renderer.cljs
@@ -11,18 +11,19 @@
 (defn component-renderer [dimensions db game-map boxes]
   (let [player (@db :player)
         npc (@db :npc)
+        ratio (/ 2 3)
         w (first dimensions)
         h (last dimensions)]
     [:div
      [:svg#canvas {:width (get-in @db [:window 0])
                    :height (get-in @db [:window 1])
                    :viewBox (str (* (/ w 2) -1) " " (* (/ h 2) -1) " " w " " h)}
-      [:g {:transform "scale(2)"}
+      [:g {:transform "scale(4)"}
        (doall
          (for [[[x y] tile] game-map]
            [:rect {:key [x y]
-                   :x (- x (first player)) :y (- y (last player))
-                   :width 0.9 :height 0.9
+                   :x (- x (first player) 0.5) :y (* (- y (last player) 0.5) ratio)
+                   :width 0.9 :height (* 0.9 ratio)
                    :fill (cond
                            (= [x y] player) "blue"
                            (= [x y] npc) "red"
